2efd8867fad1f5396b7649880fef053798b36708
sincerity628 <14785481094@163.com>
2020-01-09 14:07:15 +0800

    added register page.

--- src/pages/authentication/Signup.js
+++ src/pages/authentication/Signup.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import sha256 from 'sha256';
 import qs from 'qs';
 import api from '../../tools/api';
@@ -17,8 +18,13 @@ const initError = {
 };
 
 const Signup = () => {
+  const history = useHistory();
+
   const [user, setUser] = useState(initUser);
   const [error, setError] = useState(initError);
+  const [checkCode, setCheckCode] = useState('');
+  const [success, setSuccess] = useState(false);
+  const [btnDisabled, setBtnDisabled] = useState(false);
   const [btnLoading, setBtnLoading] = useState(false);
 
   const handleChange = e => {
@@ -31,12 +37,87 @@ const Signup = () => {
 
   const handleSubmit = e => {
     e.preventDefault();
-    console.log(user);
+    if(!user.email || !user.emailCheck || !user.username || !user.password) {
+      setError({
+        isError: true,
+        content: "请将信息填写完整"
+      });
+      return;
+    }
+    if(user.emailCheck !== checkCode) {
+      setError({
+        isError: true,
+        content: '邮箱验证码不正确'
+      });
+      return;
+    }
+    setBtnLoading(true);
+    api
+      .register(qs.stringify(user))
+      .then(res => {
+        console.log(res);
+        if(res.status === 200 && res.data.isOk) {
+          setBtnLoading(false);
+          setSuccess(true);
+          setUser(initUser);
+
+          setTimeout(() => {
+            history.push('/signin');
+          }, 1000);
+          return;
+        }
+
+        if(res.status === 200 && !res.data.isOk) {
+          setBtnLoading(false);
+          setError({
+            isError: true,
+            content: '该用户名已经注册过了噢'
+          });
+          return;
+
+        } else {
+          setBtnLoading(false);
+          setError({
+            isError: true,
+            content: '注册失败'
+          });
+          return;
+        }
+      })
+      .catch(error => {
+        setBtnLoading(false);
+        setError({
+          isError: true,
+          content: '注册失败'
+        });
+        return;
+      })
   };
 
   const emailCheck = e => {
     e.preventDefault();
-    console.log(user);
+    if(!user.email) {
+      setError({
+        isError: true,
+        content: '请填写邮箱！'
+      });
+      return;
+    }
+    let data = {
+      email: user.email
+    };
+    console.log(qs.stringify(data));
+    api
+      .emailCheck(data)
+      .then(res => {
+        if(res.status === 200 && res.data.isOk) {
+          setBtnDisabled(true);
+          setCheckCode(res.data.number);
+        };
+      })
+      .catch(error => {
+        console.log(error);
+      })
   };
 
   return (
@@ -55,7 +136,7 @@ const Signup = () => {
         </div>
 
         <div className="form-group">
-          <label htmlFor="emailCheck">验证码：</label>
+          <label htmlFor="emailCheck">邮箱验证码：</label>
           <div className="input-group">
             <input id="emailCheck" value={user.emailCheck} type="text"
               className="form-control" onChange={handleChange}
@@ -63,6 +144,7 @@ const Signup = () => {
             <div className="input-group-append">
               <button
                 className="btn btn-outline-secondary"
+                disabled={btnDisabled}
                 onClick={emailCheck}
               >获取验证码</button>
             </div>
@@ -83,7 +165,13 @@ const Signup = () => {
           />
         </div>
 
-        <button className="btn btn-dark" onClick={handleSubmit}>注册</button>
+        { error.isError && <div className="alert alert-danger">{ error.content }</div> }
+        { success && <div className="alert alert-success">注册成功，快去登录吧</div> }
+
+        <button className="btn btn-dark" onClick={handleSubmit}>
+          { btnLoading && <span className="mr-2 spinner-grow spinner-grow-sm"></span> }
+          注册
+        </button>
 
       </form>
     </div>
--- src/tools/api.js
+++ src/tools/api.js
@@ -3,6 +3,8 @@ import request from './request';
 export default {
   // authentication
   login: (data) => request.post('/signIn', data),
+  emailCheck: (data) => request.post('/emailCheck', data),
+  register: (data) => request.post('/signUp', data),
 
 
 };
