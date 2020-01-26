9900513c4e2768040a3ee6fd440ea5474e4b4d29
sincerity628 <14785481094@163.com>
2020-01-26 15:15:59 +0800

    added style.

--- src/pages/create/CreatePoem.js
+++ src/pages/create/CreatePoem.js
@@ -1,44 +1,47 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import './create.css';
 
 const CreatePoem = () => {
+  const history = useHistory();
+
   return (
     <div className="crate-poem container">
+
       <h3 className="mt-5">创作：</h3>
-      <div className="row">
+      <div className="row mt-4">
         <div className="col-sm-3">
           <div className="circle-container">
-            <Link to='/self-create'>自</Link>
-            <p className="detail-text">自由作诗</p>
+            <div className="circle-text" onClick={() => history.push('/self-create')}>自</div>
+            <p className="circle-detail-text">自由作诗</p>
           </div>
         </div>
       </div>
 
-      <h3 className="mt-5">辅助（AI作诗）：</h3>
-      <div className="row">
+      <h3 className="mt-5">辅助作诗（AI作诗）：</h3>
+      <div className="row mt-4">
         <div className="col-sm-3">
           <div className="circle-container">
-            <Link to='/cangtou'>藏</Link>
-            <p className="detail-text">藏头诗</p>
+            <div className="circle-text" onClick={() => history.push('/cangtou')}>藏</div>
+            <p className="circle-detail-text">藏头诗</p>
           </div>
         </div>
         <div className="col-sm-3">
           <div className="circle-container">
-            <Link to='/emotion'>情</Link>
-            <p className="detail-text">情感诗</p>
+            <div className="circle-text" onClick={() => history.push('/emotion')}>情</div>
+            <p className="circle-detail-text">情感诗</p>
           </div>
         </div>
         <div className="col-sm-3">
           <div className="circle-container">
-            <Link to='/style'>风</Link>
-            <p className="detail-text">风格诗</p>
+            <div className="circle-text" onClick={() => history.push('/style')}>风</div>
+            <p className="circle-detail-text">风格诗</p>
           </div>
         </div>
         <div className="col-sm-3">
           <div className="circle-container">
-            <Link to='/jielong'>龙</Link>
-            <p className="detail-text">诗词接龙</p>
+            <div className="circle-text" onClick={() => history.push('/jielong')}>龙</div>
+            <p className="circle-detail-text">诗词接龙</p>
           </div>
         </div>
       </div>
