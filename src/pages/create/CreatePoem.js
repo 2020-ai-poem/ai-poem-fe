c43f84a04cf1d0e5d995b58fb46a38e28ffb1485
sincerity628 <14785481094@163.com>
2020-01-27 17:16:59 +0800

    added images.

--- src/pages/create/CreatePoem.js
+++ src/pages/create/CreatePoem.js
@@ -6,45 +6,46 @@ const CreatePoem = () => {
   const history = useHistory();
 
   return (
-    <div className="crate-poem container">
+    <div className="create-poem container">
 
-      <h4 className="mt-5">创作：</h4>
+      <h4>辅助作诗（AI作诗）：</h4>
       <div className="row mt-4">
         <div className="col-sm-3">
           <div className="circle-container">
-            <div className="circle-text" onClick={() => history.push('/self-create')}>自</div>
-            <p className="circle-detail-text">自由作诗</p>
-          </div>
-        </div>
-      </div>
-
-      <h4 className="mt-5">辅助作诗（AI作诗）：</h4>
-      <div className="row mt-4">
-        <div className="col-sm-3">
-          <div className="circle-container">
-            <div className="circle-text" onClick={() => history.push('/cangtou')}>藏</div>
+            <div className="cangtou-img circle-text" onClick={() => history.push('/cangtou')}></div>
             <p className="circle-detail-text">藏头诗</p>
           </div>
         </div>
         <div className="col-sm-3">
           <div className="circle-container">
-            <div className="circle-text" onClick={() => history.push('/emotion')}>情</div>
+            <div className="emotion-img circle-text" onClick={() => history.push('/emotion')}></div>
             <p className="circle-detail-text">情感诗</p>
           </div>
         </div>
         <div className="col-sm-3">
           <div className="circle-container">
-            <div className="circle-text" onClick={() => history.push('/style')}>风</div>
+            <div className="style-img circle-text" onClick={() => history.push('/style')}></div>
             <p className="circle-detail-text">风格诗</p>
           </div>
         </div>
         <div className="col-sm-3">
           <div className="circle-container">
-            <div className="circle-text" onClick={() => history.push('/jielong')}>龙</div>
+            <div className="jielong-img circle-text" onClick={() => history.push('/jielong')}></div>
             <p className="circle-detail-text">诗词接龙</p>
           </div>
         </div>
       </div>
+
+      <h4 className="mt-5">创作（自主作诗）：</h4>
+      <div className="row mt-4">
+        <div className="col-sm-3">
+          <div className="circle-container">
+            <div className="self-create-img circle-text" onClick={() => history.push('/self-create')}></div>
+            <p className="circle-detail-text">自由作诗</p>
+          </div>
+        </div>
+      </div>
+      
     </div>
   );
 };
